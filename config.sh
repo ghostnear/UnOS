adff743a314cf408132d7a328eaa94732bacc53b
GhostNear <ghostnear1841@gmail.com>
2024-02-05 15:04:07 +0200

    Add support for compile_commands.json

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 SYSTEM_HEADER_PROJECTS="libc kernel"
 PROJECTS="libc kernel"
 
-export MAKE=${MAKE:-make}
+export MAKE="bear -- ${MAKE:-make}"
 export HOST=${HOST:-$(./default-host.sh)}
 
 export AR=${HOST}-ar
--- libc/include/sys/cdefs.h
+++ libc/include/sys/cdefs.h
@@ -1,6 +1,3 @@
-#ifndef _SYS_CDEFS_H
-#define _SYS_CDEFS_H 1
+#pragma once
 
 #define __unos_libc 1
-
-#endif
